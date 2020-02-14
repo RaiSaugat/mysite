78a2a19b9b56a867ce1075008e853587cf2a00e9
saugat <rai.saugat.sr@gmail.com>
2020-02-14 17:04:44 +0545

    Updates

---
+++ precache-manifest.2fd3ac612081fa4f1bf58722806df4e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f61bf8c604b1c1f9096505bd6d6decf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "224c3883c4f3983bd335",
+    "url": "/static/css/main.578adaa8.chunk.css"
+  },
+  {
+    "revision": "b8c8481ce5559ae85232",
+    "url": "/static/js/2.eef54c4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.eef54c4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224c3883c4f3983bd335",
+    "url": "/static/js/main.7f502f89.chunk.js"
+  },
+  {
+    "revision": "23601ce2d7bd68e79313",
+    "url": "/static/js/runtime-main.65c87d75.js"
+  }
+]);
